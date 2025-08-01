131f9d70fc8aea7798b9faecf6f4092552304991
BrynMorcant <brynmorcant@gmail.com>
2025-08-01 09:57:26 +0100

    Code Tweaks

--- Scripts/OnTheRailsHorsey.gd
+++ Scripts/OnTheRailsHorsey.gd
@@ -1,6 +1,7 @@
 extends PathFollow2D
 
 var the_state_of_this_fucking_horse = "idle"
+
 var horse_stats = {
 	"base speed": .001,
 	"speed": .001,
@@ -9,6 +10,7 @@ var horse_stats = {
 	"loop counter": 1,
 	"loop position": "off loop"
 }
+
 var loop_stats = {
 	"off loop": .0,#from here
 	"loop climb": .000005,#
@@ -16,6 +18,7 @@ var loop_stats = {
 	"loop complete": -.000015,# to here - Decay rates for horse speed while traversing the loop
 	"speed threshold": .5 #Speed Threshold to stay on loop
 }
+
 var loop_difficulty = {
 	1: {
 		"loop climb": .000005,
@@ -42,8 +45,10 @@ var loop_difficulty = {
 		"speed threshold": 2
 	}
 }
+
 func _awake():
 	pass
+
 func _process(delta):
 	if horse_stats["loop position"] == "off loop" && the_state_of_this_fucking_horse != "falling":
 		if Input.is_action_pressed("Boost"):
@@ -54,7 +59,8 @@ func _process(delta):
 	elif horse_stats["loop position"] != "off loop":
 		var loop_decay = loop_stats["loop position"]
 		if horse_stats["speed"] < loop_stats["speed threshold"]:
-			the_state_of_this_fucking_horse = "falling"
+			state_change("falling")
+			
 			#detatch from parent
 			#apply gravity
 		elif Input.is_action_pressed("Boost"):
@@ -64,30 +70,47 @@ func _process(delta):
 		else:
 			reset_speed(delta)
 	pass
+
+func state_change(state_name):
+	the_state_of_this_fucking_horse = state_name
+	if the_state_of_this_fucking_horse == "idle":
+		pass
+	elif the_state_of_this_fucking_horse == "trotting":
+		pass
+	elif the_state_of_this_fucking_horse == "galloping":
+		
+		pass
+	elif the_state_of_this_fucking_horse == "falling":
+		pass
+
 func horse_run(delta):
 	if horse_stats["speed"] < 0.01:
 		if the_state_of_this_fucking_horse != "trotting":
-			the_state_of_this_fucking_horse = "trotting"
+			state_change("trotting")
 			print(the_state_of_this_fucking_horse)
 	else:
 		if the_state_of_this_fucking_horse != "galloping":
-			the_state_of_this_fucking_horse = "galloping"
+			state_change("galloping")
 			print(the_state_of_this_fucking_horse)
 	progress_ratio += delta*horse_stats["speed"]
+
 func accelerate_horse():
 	if horse_stats["speed"] < horse_stats["max speed"]:
 		horse_stats["speed"] += horse_stats["acceleration"]
 	if horse_stats["speed"] > horse_stats["max speed"]:
 		horse_stats["speed"] = horse_stats ["max speed"]
+
 func apply_loop_decay(loop_decay):
 	horse_stats["speed"] -= loop_decay
+
 func reset_speed(delta):
 	if horse_stats["speed"] > horse_stats["base speed"]:
 		horse_run(delta)
 		horse_stats["speed"] -= horse_stats["acceleration"] * 3
 		if horse_stats["speed"] <= horse_stats["base speed"]:
-			the_state_of_this_fucking_horse = "idle"
+			state_change("idle")
 			print(the_state_of_this_fucking_horse)
+
 func loop_complete():
 	horse_stats["loop counter"]+=1
 	for stat in loop_stats:
@@ -95,6 +118,7 @@ func loop_complete():
 			continue
 		loop_stats[stat] = loop_difficulty[horse_stats["loop counter"]["stat"]]
 		print(loop_difficulty[horse_stats["loop Counter"]["stat"]])
+
 func horse_upgrade(): #incomplete
 	
 	#acceleration += .00001
