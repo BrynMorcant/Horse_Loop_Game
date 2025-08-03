4d7192b28e07cdb82b8e3279c5c9335261b052a6
Messedup Murphy <>
2025-08-03 14:39:38 +0100

    updated ui sounds

--- Scripts/menu_control.gd
+++ Scripts/menu_control.gd
@@ -14,22 +14,30 @@ func _on_link_button_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	credits.visible = false
 	main_menu.visible = true
-	$HBopla
 	pass # Replace with function body.
 
 
 func _on_game_start_pressed() -> void:
+	$Click.play()
 	get_tree().change_scene_to_file("res://Scenes/test_course.tscn")
-	
 	pass # Replace with function body.
-
-
+func _on_game_start_mouse_entered() -> void:
+	$Hover.play()
+	
 func _on_credits_pressed() -> void:
+	$Click.play()
 	main_menu.visible = false
 	credits.visible = true
 	pass # Replace with function body.
-
-
+func _on_credits_mouse_entered() -> void:
+	$Hover.play()
+	pass # Replace with function body.
+	
 func _on_quit_pressed() -> void:
+	$Click.play()
 	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_quit_mouse_entered() -> void:
+	$Hover.play()
 	pass # Replace with function body.
